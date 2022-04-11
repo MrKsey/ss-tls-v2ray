e71d485a9fe1fcd9038715dc53a94604d9446672
ksey <ksey3000@gmail.com>
2022-04-11 12:22:51 +0300

    Update ss.sh

--- config/client/ss.sh
+++ config/client/ss.sh
@@ -14,8 +14,8 @@ stop() {
 
 status() {
     STATUS=$(lsof -n -P -i | grep "sslocal" | grep ":$SS_LOCAL_PORT"); [ -z "$STATUS" ] && \
-    printf "${RED}Client sslocal is not started.${NC}\n The network port $SS_LOCAL_PORT may already be in use. Change port parameter local_port in ../client/ss.json and restart client" || 
-    printf "${GREEN}Client sslocal is running:${NC}\n"; lsof -n -P -i | grep "sslocal" | grep ":$SS_LOCAL_PORT"
+    printf "${RED}Client ShadowSocks is not started.${NC}\n The network port $SS_LOCAL_PORT may already be in use. Change port parameter local_port in ../client/ss.json and restart client" || 
+    printf "${GREEN}Client ShadowSocks is running:${NC}\n"; lsof -n -P -i | grep "sslocal" | grep ":$SS_LOCAL_PORT"
 }
 
 case "$1" in 
