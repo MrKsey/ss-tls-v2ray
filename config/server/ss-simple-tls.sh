e41b21eef77979c570d4fe7664a8ce4010883a2d
ksey <ksey3000@gmail.com>
2022-04-11 12:33:40 +0300

    Update ss-simple-tls.sh

--- config/server/ss-simple-tls.sh
+++ config/server/ss-simple-tls.sh
@@ -14,7 +14,7 @@ stop() {
 
 status() {
     STATUS=$(lsof -i -P -n | grep simple-tl); [ -z "$STATUS" ] && \
-    printf "${RED}Server SIMPLE-TLS is not started.${NC}\n The network port $SIMPLE_TLS_SERVER_PORT may already be in use. Change port parameter -b in ../server/ss-simple-tls.sh and restart server\n" || 
+    printf "${RED}Server SIMPLE-TLS is not started.${NC}\nThe network port ${RED}$SIMPLE_TLS_SERVER_PORT${NC} may already be in use. Change port parameter ${RED}-b${NC} in ../server/ss-simple-tls.sh and restart server\n" || 
     printf "${GREEN}Server SIMPLE-TLS is running:${NC}\n"; lsof -i -P -n | grep simple-tl
 }
 
