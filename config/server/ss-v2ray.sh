32d676b560c12491d32cddffa672467018079bc9
ksey <ksey3000@gmail.com>
2022-04-11 10:45:15 +0300

    Update ss-v2ray.sh

--- config/server/ss-v2ray.sh
+++ config/server/ss-v2ray.sh
@@ -14,8 +14,8 @@ stop() {
 
 status() {
     STATUS=$(lsof -i -P -n | grep v2ray); [ -z "$STATUS" ] && \
-    echo "Server v2ray is not started. The network port $V2RAY_SERVER_PORT may already be in use. Change port parameter -localPort in ../server/ss-v2ray.sh and restart server" || 
-    echo "Server v2ray is running:"; lsof -i -P -n | grep v2ray
+    echo "${RED}Server v2ray is not started.${NC} The network port $V2RAY_SERVER_PORT may already be in use. Change port parameter -localPort in ../server/ss-v2ray.sh and restart server" || 
+    echo "${GREEN}Server v2ray is running:${NC}"; lsof -i -P -n | grep v2ray
 }
 
 case "$1" in 
