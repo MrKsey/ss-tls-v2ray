b9120b38b85be45aed3395418c10f48aa54b785a
ksey <ksey3000@gmail.com>
2022-04-14 21:44:36 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -61,6 +61,12 @@ sed -i "/^UPDATE_SCHEDULE=/{h;s/=.*/=${UPDATE_SCHEDULE}/};\${x;/^$/{s//UPDATE_SC
 sed -i -E "s/UPDATE_SCHEDULE=(.*)/UPDATE_SCHEDULE=\"\1\"/" $CONFIG_PATH/config.ini
 sed -i "s/\"\"/\"/g" $CONFIG_PATH/config.ini
 
+# Scheduled updates
+if [ ! -z "$UPDATE_SCHEDULE" ]; then
+    echo "$UPDATE_SCHEDULE /update.sh >> /var/log/cron.log 2>&1" | crontab -
+    cron -f >> /var/log/cron.log 2>&1&
+fi
+
 #  MODE
 [ -z "$MODE" ] && export MODE=server
 [ "$MODE" != "server" ] && export MODE=client
