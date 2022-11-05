22f591c1eae9ed8b7522a35b2051c40692109374
ksey <17622530+MrKsey@users.noreply.github.com>
2022-11-05 13:05:24 +0300

    Update restart_svc.sh

--- restart_svc.sh
+++ restart_svc.sh
@@ -46,7 +46,7 @@ else
 fi
 
 # Start new monitoring config.ini
-ls /etc/shadowsocks/config.ini | entr -psz 'echo "$(date): config.ini changed. Applying new settings..." && /config.sh && /restart_svc.sh' &
+ls /etc/shadowsocks/config.ini | entr -npsz 'echo "$(date): config.ini changed. Applying new settings..." && /config.sh && /restart_svc.sh' &
 
 echo " "
 echo "=================================================="
